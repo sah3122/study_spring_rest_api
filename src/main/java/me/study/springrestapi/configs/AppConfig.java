cde2e81c5f40486d920f7fdece295533545d7e5b
dongchul <sah3122@naver.com>
2020-01-07 21:54:26 +0900

    Add Spring Security Form Login

--- src/main/java/me/study/springrestapi/configs/AppConfig.java
+++ src/main/java/me/study/springrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package me.study.springrestapi.configs;
 
+import me.study.springrestapi.accounts.Account;
+import me.study.springrestapi.accounts.AccountRole;
+import me.study.springrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -16,5 +24,23 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account dongchul = Account.builder()
+                        .email("dongchul@email.com")
+                        .password("dongchul")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(dongchul);
+            }
+        };
     }
 }
--- src/test/java/me/study/springrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/study/springrestapi/accounts/AccountServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -32,6 +33,9 @@ public class AccountServiceTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     public void findByUsername() throws Exception {
         //given
@@ -43,14 +47,13 @@ public class AccountServiceTest {
                 .password(password)
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
-
-        accountRepository.save(account);
+        this.accountService.saveAccount(account);
 
         //when
         UserDetailsService userDetailsService = (UserDetailsService)accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername("dongchul@email.com");
         //then
-        assertThat(userDetails.getPassword()).isEqualTo(password);
+        assertThat(this.passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
     }
 
     @Test
